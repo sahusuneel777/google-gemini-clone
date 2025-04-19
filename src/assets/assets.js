faa5cb6d4288b84bc652867a21313a5d8df3ce3e
suneel <suneel.sahu@kimshospitals.com>
2025-04-19 10:19:49 +0530

    avatar added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -9,6 +9,7 @@ import gallery_icon from './gallery_icon.png'
 import mic_icon from './mic_icon.png'
 import user_icon from './user_icon.png'
 import user from './user.jpg'
+import avatar from './avatar.jpg'
 import youtube_icon from './youtube_icon.png'
 import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
@@ -32,4 +33,5 @@ export const assets = {
 	send_icon,
 	gemini_icon,
 	user,
+	avatar
 };
